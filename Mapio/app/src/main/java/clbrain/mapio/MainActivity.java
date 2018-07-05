b8e373cf68c6617c76848363581dad918c83919d
KirillR911 <KirikR911@gmail.com>
2018-07-05 15:43:45 +0300

    New New Design

--- Mapio/app/src/main/java/clbrain/mapio/MainActivity.java
+++ Mapio/app/src/main/java/clbrain/mapio/MainActivity.java
@@ -257,7 +257,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         // Turn on the My Location layer and the related control on the map.
         updateLocationUI();
-
         // Get the current location of the device and set the position of the map.
         getDeviceLocation();
         //Отрисовка
